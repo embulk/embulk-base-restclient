ca497a453e64cc2ba9c28e184bbc0bb028cc1503
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-02-01 17:16:15 -0800

    Use Jackson's |as*| methods to recognize JSON values as Java (Embulk) values. (#12)

--- src/main/java/org/embulk/base/restclient/record/JacksonServiceValue.java
+++ src/main/java/org/embulk/base/restclient/record/JacksonServiceValue.java
@@ -2,6 +2,28 @@ package org.embulk.base.restclient.record;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+/**
+ * JacksonServiceValue represents a value in a JSON response to be converted to an Embulk column value.
+ *
+ * {@code JacksonServiceValue} depends on Jackson {@code JsonNode}'s {@code as*} methods if type
+ * conversion is needed.
+ *
+ * For example with Jackson 2.5.0, the JSON below is recognized as {@code boolean} {@code true}.
+ * <pre>{@code
+ * { "flag": "true" }
+ * }</pre>
+ *
+ * The other JSON below however cannot be recognized as Embulk's {@code boolean} {@code true}
+ * because Jackson 2.5.0 recognizes only {@code "true"} and {@code "false"}.
+ * <pre>{@code
+ * { "flag": "True" }
+ * }</pre>
+ *
+ * @see https://github.com/FasterXML/jackson-databind/blob/jackson-databind-2.5.0/src/main/java/com/fasterxml/jackson/databind/node/TextNode.java#L177-L189
+ *
+ * Implement another set of {@code ServiceValue} and {@code ServiceRecord} if a different style of
+ * type conversion is required.
+ */
 public class JacksonServiceValue
         extends ServiceValue
 {
@@ -19,25 +41,25 @@ public class JacksonServiceValue
     @Override
     public boolean booleanValue()
     {
-        return value.booleanValue();
+        return value.asBoolean();
     }
 
     @Override
     public double doubleValue()
     {
-        return value.doubleValue();
+        return value.asDouble();
     }
 
     @Override
     public org.msgpack.value.Value jsonValue(org.embulk.spi.json.JsonParser jsonParser)
     {
-        return jsonParser.parse(value.toString());
+        return jsonParser.parse(value.asText());
     }
 
     @Override
     public long longValue()
     {
-        return value.longValue();
+        return value.asLong();
     }
 
     @Override
