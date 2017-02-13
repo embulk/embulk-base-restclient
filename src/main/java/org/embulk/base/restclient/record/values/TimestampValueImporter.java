0f607cc225cfea75a3e0d31d798cbfe2e44ad4ef
Dai MIKURUBE <dmikurube@treasure-data.com>
2017-02-13 16:06:45 +0900

    Make |*ValueImporter| to throw Exceptions which are easier to investigate.

--- src/main/java/org/embulk/base/restclient/record/values/TimestampValueImporter.java
+++ src/main/java/org/embulk/base/restclient/record/values/TimestampValueImporter.java
@@ -1,6 +1,7 @@
 package org.embulk.base.restclient.record.values;
 
 import org.embulk.spi.Column;
+import org.embulk.spi.DataException;
 import org.embulk.spi.PageBuilder;
 import org.embulk.spi.time.TimestampParser;
 
@@ -21,12 +22,16 @@ public class TimestampValueImporter
     @Override
     public void findAndImportValue(ServiceRecord record, PageBuilder pageBuilder)
     {
-        ServiceValue value = findValue(record);
-        if (value == null || value.isNull()) {
-            pageBuilder.setNull(getColumnToImport());
-        }
-        else {
-            pageBuilder.setTimestamp(getColumnToImport(), value.timestampValue(timestampParser));
+        try {
+            ServiceValue value = findValue(record);
+            if (value == null || value.isNull()) {
+                pageBuilder.setNull(getColumnToImport());
+            }
+            else {
+                pageBuilder.setTimestamp(getColumnToImport(), value.timestampValue(timestampParser));
+            }
+        } catch (Exception ex) {
+            throw new DataException("Failed to import a value for column: " + getColumnToImport().getName() + " (" + getColumnToImport().getType().getName() + ")", ex);
         }
     }
 
