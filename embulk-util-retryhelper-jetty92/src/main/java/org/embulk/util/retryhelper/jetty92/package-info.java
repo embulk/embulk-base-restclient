69820ef5a3778288c1ef48ce63a3994d52caebcd
shroman <rshtykh@yahoo.com>
2019-09-22 20:05:41 -0700

    Deprecated Jetty 9.2, as it's reached its end of life. closes #109.

--- embulk-util-retryhelper-jetty92/src/main/java/org/embulk/util/retryhelper/jetty92/package-info.java
+++ embulk-util-retryhelper-jetty92/src/main/java/org/embulk/util/retryhelper/jetty92/package-info.java
@@ -1,5 +1,5 @@
 /**
- * @deprecated Use {@code org.embulk.util.retryhelper.jetty93} instead.
+ * Use {@code org.embulk.util.retryhelper.jetty93} instead.
  */
 @Deprecated
 package org.embulk.util.retryhelper.jetty92;
