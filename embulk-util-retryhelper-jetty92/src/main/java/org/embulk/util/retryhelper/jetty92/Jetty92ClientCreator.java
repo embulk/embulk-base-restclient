50451b3c6ee7ffc88b49c002115e5e93117849d9
Dai MIKURUBE <dmikurube@treasure-data.com>
2020-07-22 17:55:31 +0900

    Remove embulk-util-*
    They are exported to other repositories:
    * https://github.com/embulk/embulk-util-retryhelper
    * https://github.com/embulk/embulk-util-throttle-helper

--- embulk-util-retryhelper-jetty92/src/main/java/org/embulk/util/retryhelper/jetty92/Jetty92ClientCreator.java
+++
@@ -1,8 +0,0 @@
-package org.embulk.util.retryhelper.jetty92;
-
-import org.eclipse.jetty.client.HttpClient;
-
-public interface Jetty92ClientCreator
-{
-    HttpClient createAndStart() throws Exception;
-}
--- embulk-util-retryhelper-jetty93/src/main/java/org/embulk/util/retryhelper/jetty93/Jetty93ClientCreator.java
+++
@@ -1,8 +0,0 @@
-package org.embulk.util.retryhelper.jetty93;
-
-import org.eclipse.jetty.client.HttpClient;
-
-public interface Jetty93ClientCreator
-{
-    HttpClient createAndStart() throws Exception;
-}
