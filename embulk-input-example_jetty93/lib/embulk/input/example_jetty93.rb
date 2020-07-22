9bb22e9a828605ec042d0c9c270a84ba24e37883
Dai MIKURUBE <dmikurube@treasure-data.com>
2020-07-22 18:19:30 +0900

    Build embulk-base-restclient and example plugins with a modern Gradle and Embulk style
    * Upgrades Gradle to 6.5.1.
    * Enables dependency lock.
    * Builds examples with the "org.embulk.embulk-plugins" Gradle plugin.

--- embulk-input-example_jetty93/lib/embulk/input/example_jetty93.rb
+++
@@ -1,3 +0,0 @@
-Embulk::JavaPlugin.register_input(
-  "example_jetty93", "org.embulk.input.example_jetty93.ExampleJetty93InputPlugin",
-  File.expand_path('../../../../classpath', __FILE__))
--- embulk-output-example/lib/embulk/output/example.rb
+++
@@ -1,3 +0,0 @@
-Embulk::JavaPlugin.register_output(
-  "example", "org.embulk.output.example.ExampleOutputPlugin",
-  File.expand_path('../../../../classpath', __FILE__))
